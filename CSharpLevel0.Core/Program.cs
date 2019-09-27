8151c33b448a16f978027a7d047202ae273fafc7
v h <szscancer@gmail.com>
2019-09-27 08:40:34 +0800

    添加core项目

---
+++ CSharpLevel0.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpLevel0.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
